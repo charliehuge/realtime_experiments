ee325b1d8a97f169811b46bd98c336fcab1514c9
github-action-benchmark <github@users.noreply.github.com>
2022-02-06 11:44:11 +0000

    add rt_thread_cb Benchmark (googlecpp) benchmark result for ebc223c30dcb0f8092d839084701df04927dbd59

--- dev/bench/rt_thread_cb/data.js
+++ dev/bench/rt_thread_cb/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644147398659,
+  "lastUpdate": 1644147851743,
   "repoUrl": "https://github.com/charliehuge/realtime_experiments",
   "entries": {
     "rt_thread_cb Benchmark": [
@@ -78,6 +78,84 @@ window.BENCHMARK_DATA = {
             "value": 32.60714398061985,
             "unit": "ns/iter",
             "extra": "iterations: 22156975\ncpu: 32.59986979269505 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "charliehuge@users.noreply.github.com",
+            "name": "Charlie Huguenard",
+            "username": "charliehuge"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ebc223c30dcb0f8092d839084701df04927dbd59",
+          "message": "Update README.md",
+          "timestamp": "2022-02-06T06:43:09-05:00",
+          "tree_id": "3ccbf3082bff5e61969245c68535807cf5443e59",
+          "url": "https://github.com/charliehuge/realtime_experiments/commit/ebc223c30dcb0f8092d839084701df04927dbd59"
+        },
+        "date": 1644147850834,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "ThingWithCb_SetUnset<ThingWithCbAndSpinlock<CbData>>",
+            "value": 25.627004345079072,
+            "unit": "ns/iter",
+            "extra": "iterations: 27605713\ncpu: 25.352875326929613 ns\nthreads: 1"
+          },
+          {
+            "name": "ThingWithCb_SetUnset<ThingWithCbAndSPSCQ<CbData>>",
+            "value": 3.6268267991440335,
+            "unit": "ns/iter",
+            "extra": "iterations: 193157319\ncpu: 3.6227143947882197 ns\nthreads: 1"
+          },
+          {
+            "name": "ThingWithCb_SetUnset<ThingWithCbAndEyalAmirFifo<CbData>>",
+            "value": 52.13365606753008,
+            "unit": "ns/iter",
+            "extra": "iterations: 13511635\ncpu: 51.78061722360026 ns\nthreads: 1"
+          },
+          {
+            "name": "ThingWithCb_Process<ThingWithCbAndSpinlock<CbData>>",
+            "value": 190.9979901343209,
+            "unit": "ns/iter",
+            "extra": "iterations: 3501229\ncpu: 190.07611327336778 ns\nthreads: 1"
+          },
+          {
+            "name": "ThingWithCb_Process<ThingWithCbAndSPSCQ<CbData>>",
+            "value": 264.20348178825395,
+            "unit": "ns/iter",
+            "extra": "iterations: 2671271\ncpu: 261.9609916028738 ns\nthreads: 1"
+          },
+          {
+            "name": "ThingWithCb_Process<ThingWithCbAndEyalAmirFifo<CbData>>",
+            "value": 377.8739630011144,
+            "unit": "ns/iter",
+            "extra": "iterations: 1868975\ncpu: 376.33055551839914 ns\nthreads: 1"
+          },
+          {
+            "name": "ThingWithCb_ProcessNoContention<ThingWithCbAndSpinlock<CbData>>",
+            "value": 12.860749945264404,
+            "unit": "ns/iter",
+            "extra": "iterations: 54443440\ncpu: 12.858412326627425 ns\nthreads: 1"
+          },
+          {
+            "name": "ThingWithCb_ProcessNoContention<ThingWithCbAndSPSCQ<CbData>>",
+            "value": 9.497412818921545,
+            "unit": "ns/iter",
+            "extra": "iterations: 73592259\ncpu: 9.497391566686382 ns\nthreads: 1"
+          },
+          {
+            "name": "ThingWithCb_ProcessNoContention<ThingWithCbAndEyalAmirFifo<CbData>>",
+            "value": 32.908016722624986,
+            "unit": "ns/iter",
+            "extra": "iterations: 21275128\ncpu: 32.906382513891316 ns\nthreads: 1"
           }
         ]
       }
